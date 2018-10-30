dad10889b0839137dfb4c69e2717034a52e14703
Jeonghoon Seong <acoustically1011@gmail.com>
2018-10-30 11:43:23 +0000

    create layout components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './todo-list/main/main.component';
+import { SignInComponent } from './sign/sign-in/sign-in.component';
+import { SignUpComponent } from './sign/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
